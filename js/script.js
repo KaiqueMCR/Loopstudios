3d8ef36d57daa5ae1f9fa79419d65551c7803591
Kaiquemcr <Kaiquedev404@gmail.com>
2022-04-09 19:21:45 -0300

    file & mod: add creations,components ,refactored

--- js/script.js
+++ js/script.js
@@ -3,3 +3,9 @@ $('.mobileMenu').click(() => {
 	$('.close').toggleClass('hidden')
 	$('.open').toggleClass('hidden')
 })
+
+$('.navbar').click(() => {
+	$('.navbar').animate({ width: 'toggle' }, 'fast')
+	$('.close').toggleClass('hidden')
+	$('.open').toggleClass('hidden')
+})
