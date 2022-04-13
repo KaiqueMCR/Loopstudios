6fca0266be565c58ffaf77ff155affe7bf75dd05
Kaiquemcr <Kaiquedev404@gmail.com>
2022-04-13 16:51:51 -0300

    mod: add scrollreveal lib

---
+++ js/reveal.js
@@ -0,0 +1,48 @@
+ScrollReveal().reveal('.logo', {
+	scale: 0
+})
+ScrollReveal().reveal('.menu_item', {
+	scale: 0,
+	interval: 100
+})
+
+ScrollReveal().reveal('.heading_titleWrapper', {
+	scale: 0
+})
+ScrollReveal().reveal('.about_img', {
+	distance: '500px',
+	origin: 'left',
+	easing: 'ease-out'
+})
+ScrollReveal().reveal('.about_textWrapper', {
+	distance: '100px',
+	origin: 'right',
+	easing: 'ease-out'
+})
+
+ScrollReveal().reveal('.creations_titleWrapper .title', {
+	scale: 0
+})
+ScrollReveal().reveal('.seeAll_btn.hidden', {
+	scale: 0
+})
+ScrollReveal().reveal('.seeAll_btn', {
+	scale: 0
+})
+ScrollReveal().reveal('.card', {
+	scale: 0,
+	interval: 100
+})
+
+ScrollReveal().reveal('.social', {
+	scale: 0,
+	interval: 100
+})
+
+ScrollReveal().reveal('.footer_menu_item', {
+	scale: 0,
+	interval: 100
+})
+ScrollReveal().reveal('.copyright', {
+	scale: 0
+})
