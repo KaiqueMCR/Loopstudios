b702baab43015b5272cf53fd216945e535d47005
Kaiquemcr <Kaiquedev404@gmail.com>
2022-04-12 22:45:10 -0300

    file & mod: responsive on main and header contents

--- js/script.js
+++ js/script.js
@@ -9,3 +9,20 @@ $('.navbar').click(() => {
 	$('.close').toggleClass('hidden')
 	$('.open').toggleClass('hidden')
 })
+
+const images = $('.card_img').toArray()
+
+function setImages() {
+	if (window.innerWidth >= 992) {
+		images.forEach(image => {
+			image.src = image.dataset.srcsetDesktop
+		})
+	} else {
+		images.forEach(image => {
+			image.src = image.dataset.srcsetMobile
+		})
+	}
+}
+
+$(window).on('resize', setImages)
+$(document).ready(setImages)
